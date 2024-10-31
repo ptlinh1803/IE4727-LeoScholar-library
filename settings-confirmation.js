dd279bb3dd689a34cd7202363cc0fe64b14f3d34
Tran Huu Nghia (Nikola) <nghtrn.work@gmail.com>
2024-11-01 01:03:50 +0800

    Edited JS function to make sure submission is canceled when user selects 'No'

--- settings-confirmation.js
+++ settings-confirmation.js
@@ -1,23 +1,11 @@
 function confirmUpdateCategories() {
-  if (confirm("Are you sure you want to update your favorite categories?")) {
-    // Redirect to logout_member.php if the user clicked "Yes"
-    window.location.href = "update-fav-categories.php";
-  }
-  // Else do nothing
+  return (confirm("Are you sure you want to update your favorite categories?"));
 }
 
 function confirmUpdateInfo() {
-  if (confirm("Are you sure you want to update your personal particulars?")) {
-    // Redirect to update-info.php if the user clicked "Yes"
-    window.location.href = "update-info.php";
-  }
-  // Else do nothing
+  return (confirm("Are you sure you want to update your personal particulars?"));
 }
 
 function confirmUpdatePassword() {
-  if (confirm("Are you sure you want to update your password?")) {
-    // Redirect to update-password.php if the user clicked "Yes"
-    window.location.href = "update-password.php";
-  }
-  // Else do nothing
+  return (confirm("Are you sure you want to update your password?"));
 }
