e5a9599a346f3b8e8ee7b232b9f639680020b91a
Tran Huu Nghia (Nikola) <nghtrn.work@gmail.com>
2024-11-03 02:09:10 +0800

    Remove all error display from the scripts to avoid displaying error when session is already started

--- availability-table.php
+++ availability-table.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 // Create new session if it has not been created yet
 if (session_status() == PHP_SESSION_NONE) {
   session_start();
